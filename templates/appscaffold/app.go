83dec0e7b2708aebcdbb7e88ea4af0d422911095
oceanho <gzhehai@foxmail.com>
2020-08-04 00:45:57 +0800

    Add scaffold files.

---
+++ templates/appscaffold/app.go
@@ -0,0 +1,42 @@
+package uap
+
+import (
+	"github.com/oceanho/gw"
+)
+
+
+//
+// Impl gw.App interface at here.
+// reference: https://github.com/oceanho/gw/wiki/Scaffold-Guides#6-appgo
+//
+
+type App struct {
+}
+
+func New() App {
+	return App{}
+}
+
+
+func (a App) Name() string {
+	// return your app name
+	// Example: return "gw.uap"
+	return "my-app"
+}
+
+func (a App) Router() string {
+	// return your app router group name.
+	return "my-app"
+}
+
+func (a App) Register(router *gw.RouterGroup) {
+	// your app routers
+}
+
+func (a App) Migrate(store gw.Store) {
+	// your database migrations
+}
+
+func (a App) Use(opt *gw.ServerOption) {
+	// modify serverOptions if your have necessary.
+}
---
+++ templates/appscaffold/biz/xx.go
@@ -0,0 +1,6 @@
+package biz
+
+//
+// written your Logistics at here.
+// reference: https://github.com/oceanho/gw/wiki/Scaffold-Guides#2-bizxxgo
+//
