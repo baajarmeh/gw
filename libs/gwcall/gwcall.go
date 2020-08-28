680d2ce59d2b23ed3d9f6bdcfd965157bf489156
Ocean <gzhehai@foxmail.com>
2020-08-29 04:17:13 +0800

    Fix issues

--- libs/gwcall/gwcall.go
+++ libs/gwcall/gwcall.go
@@ -8,14 +8,14 @@ import (
 var ErrorTimeout = fmt.Errorf("timeout")
 
 func Call(f func(), timeout time.Duration) error {
-	var ch = make(chan bool, 1)
-	go func() {
-		defer close(ch)
+	var done = make(chan bool, 1)
+	defer close(done)
+	go func(ch chan bool) {
 		f()
 		ch <- true
-	}()
+	}(done)
 	select {
-	case _, _ = <-ch:
+	case <-done:
 		return nil
 	case <-time.After(timeout):
 		return ErrorTimeout
